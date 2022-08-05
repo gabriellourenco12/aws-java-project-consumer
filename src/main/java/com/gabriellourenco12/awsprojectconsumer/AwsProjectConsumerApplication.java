86d9ef81029efeb75f7a448a78350a521d096b83
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-08-04 23:28:49 -0300

    commit de adequacao

---
+++ src/main/java/com/gabriellourenco12/awsprojectconsumer/AwsProjectConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gabriellourenco12.awsprojectconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsProjectConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabriellourenco12/awsprojectconsumer/AwsProjectConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellourenco12.awsprojectconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
