4a4bcbe5da4ffa7bc35e64bdd88e615674e1d852
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-08-15 20:02:04 -0300

    remocao de imports desnecessarios

--- src/main/java/com/gabriellourenco12/awsprojectconsumer/config/local/JmsConfigLocal.java
+++ src/main/java/com/gabriellourenco12/awsprojectconsumer/config/local/JmsConfigLocal.java
@@ -6,7 +6,6 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
